6372d66463edaea4ed25f4074b081886deb7d2ef
dlepera <dlepera@ultra-linux>
2014-08-08 00:08:18 -0300

    Adicionada configuraçao para servidor de testes

---
+++ aplicacao/etc/testes/painel.conf.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @Autor	: Diego Lepera
+ * @Projeto	: FrameworkDL
+ * @Data	: 15/05/2014 16:35:23
+ */
+
+# Diretórios
+$this->_dir_raiz('/buffet-salakaboom/');
+self::$dir_css = '/aplicacao/css/painel/';
+
+# Configuração do sistema
+$this->_carregaridioma('pt-BR');
+self::$ap_nome      = 'Painel-DL';
+self::$ap_slogan    = 'Gerenciador de conteúdo';
+self::$conf_versao  = '<span class="dl-versao">1.1 BETA</span>';
+
+# Configurações do banco de dados
+$this->bd_usuario   = 'u260168511_salak';
+$this->bd_senha     = '$@l4K@';
+$this->bd_base      = 'salakaboom';
+
+# Configuração dos plugins
+self::$plugin_formulario_tema   = 'colorido';
+self::$plugin_paginacao_tema    = 'painel-dl';
---
+++ aplicacao/etc/testes/website.conf.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @Autor	: Diego Lepera
+ * @Projeto	: FrameworkDL
+ * @Data	: 15/05/2014 16:35:23
+ */
+
+# Diretórios
+$this->_dir_raiz('/buffet-salakaboom/');
+self::$dir_css = '/aplicacao/css/site/';
+
+# Configuração do sistema
+$this->_carregaridioma('pt-BR');
+self::$ap_nome      = 'Buffet Salakaboom';
+self::$ap_slogan    = 'Oi! Meu nome é Salakaboom. Seja bem-vindo a um mundo de magia e diversão!';
+self::$conf_separador_titulo = ' :|: ';
+
+# Configurações do banco de dados
+$this->bd_usuario   = 'u260168511_salak';
+$this->bd_senha     = '$@l4K@';
+$this->bd_base      = 'salakaboom';
+
+# Configurações de data
+self::$dh_formato_hora = 'H:i';
+
+# Configuração dos plugins
+self::$plugin_formulario_tema   = 'salakaboom';
+self::$plugin_galeria_tema      = 'salakaboom';
